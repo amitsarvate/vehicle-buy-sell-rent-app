3bfd7ddc9e03974fd8da5bc70cbdd3b1b42a1daf
Amit Sarvate <amitsarvate@Amits-MacBook-Air.local>
2024-11-14 23:50:57 -0500

    added api fetch populated drop downs to sell page

--- lib/SellCarPage.dart
+++ lib/SellCarPage.dart
@@ -1,46 +1,53 @@
 import 'package:flutter/material.dart';
 import 'SellPost.dart';
 import 'SellPostModel.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+import 'makeAndModelAPIFetch.dart' as MMAPI;
+import 'package:dropdown_search/dropdown_search.dart';
 
-class SellCarPage extends StatefulWidget{
-
+class SellCarPage extends StatefulWidget {
   @override
   _SellCarPageState createState() => _SellCarPageState();
-
 }
 
-//temporary set of models, can add functionality later
-enum Models {
-  hyundai('Hyundai'),
-  ford('Ford'),
-  toyota('Toyota'),
-  honda('Honda'),
-  chevrolet('Chevrolet'),
-  kia('Kia'),
-  mazda('Mazda');
-
-  const Models(this.models);
-  final String models;
-}
-
-class _SellCarPageState extends State<SellCarPage>{
-
-  final TextEditingController modelController = TextEditingController();
+class _SellCarPageState extends State<SellCarPage> {
   final TextEditingController yearController = TextEditingController();
   final TextEditingController priceController = TextEditingController();
   final TextEditingController descriptionController = TextEditingController();
   final TextEditingController imageController = TextEditingController();
-  Models? _selectedModel;
-  SellPostModel sellPostModel = SellPostModel() ;
 
-  void _handleAddToListings() async{
-    if (modelController.text.isEmpty ||
+  Map<String, List<MMAPI.Model>> makesAndModels = {};
+  List<String> makes = [];
+  String? selectedMake;
+  List<MMAPI.Model> models = [];
+  MMAPI.Model? selectedModel;
+
+  SellPostModel sellPostModel = SellPostModel();
+
+  @override
+  void initState() {
+    super.initState();
+    _loadMakesAndModels();
+  }
+
+  Future<void> _loadMakesAndModels() async {
+    try {
+      final fetchedMakesAndModels = await MMAPI.fetchMakesAndModels();
+      setState(() {
+        makesAndModels = fetchedMakesAndModels;
+        makes = makesAndModels.keys.toList();
+      });
+    } catch (e) {
+      print('Failed to load makes and models: $e');
+    }
+  }
+
+  void _handleAddToListings() async {
+    if (selectedMake == null ||
+        selectedModel == null ||
         yearController.text.isEmpty ||
         priceController.text.isEmpty ||
         descriptionController.text.isEmpty ||
         imageController.text.isEmpty) {
-      // Show an error or return if any field is empty
       ScaffoldMessenger.of(context).showSnackBar(
         const SnackBar(content: Text('Please fill all the fields')),
       );
@@ -48,17 +55,16 @@ class _SellCarPageState extends State<SellCarPage>{
     }
 
     // Parse inputs
-    String model = _selectedModel?.models ?? "";
+    String make = selectedMake!;
+    String model = selectedModel!.modelName;
     int year = int.tryParse(yearController.text) ?? 0;
     int price = int.tryParse(priceController.text) ?? 0;
-    String description = descriptionController.text??"";
+    String description = descriptionController.text;
     String image = imageController.text;
 
     // Create a SellPost object
-    // Parse inputs
-
-    // Create a map from the form data
     Map<String, dynamic> postData = {
+      'make': make,
       'model': model,
       'year': year,
       'price': price,
@@ -66,182 +72,152 @@ class _SellCarPageState extends State<SellCarPage>{
       'image': image,
     };
 
-    // Create a SellPost object from the map
     SellPost newPost = SellPost.fromMap(postData);
 
-    // Clear the input fields after creating the SellPost object
     try {
       await sellPostModel.insertSellPost(newPost);
 
       // Clear the input fields after successful addition
-      modelController.clear();
+      setState(() {
+        selectedMake = null;
+        selectedModel = null;
+        models = [];
+      });
       yearController.clear();
       priceController.clear();
       descriptionController.clear();
       imageController.clear();
 
-      // Show a success message
       ScaffoldMessenger.of(context).showSnackBar(
         const SnackBar(content: Text('Car added to listings successfully!')),
       );
       print(newPost.toMap());
     } catch (e) {
-      // Show an error message if something goes wrong
       ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(content: Text('Failed to add car to listings: $e')),
       );
     }
-
-
-
-
   }
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        iconTheme: const IconThemeData(
-          color: Colors.white,
-        ),
         title: const Text(
-            'Sell Car',
-            style: TextStyle(
-                color: Colors.white
-            )
+          'Sell Car',
+          style: TextStyle(color: Colors.white),
         ),
+        iconTheme: const IconThemeData(color: Colors.white),
         backgroundColor: const Color(0xffe23636),
       ),
       body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 50),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            DropdownMenu<Models>(
-              width: 300,
-              controller: modelController,
-              requestFocusOnTap: true,
-              label: const Text('Models'),
-              onSelected: (Models? models) {
-                setState(() {
-                  _selectedModel = models;
-                });
-              },
-              dropdownMenuEntries: Models.values.map<DropdownMenuEntry<Models>>((Models models) {
-                return DropdownMenuEntry<Models>(
-                  value: models,
-                  label: models.models,
-                );
-              },
-              ).toList(),
-            ),
-            const SizedBox(height: 30),
-            Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                  SizedBox(
-                    width: 300,
-                    child: TextField(
-                      controller: yearController,
-                      decoration: const InputDecoration(
-                        labelText: 'Year',
-                        border: OutlineInputBorder(),
-                      ),
-                      keyboardType: TextInputType.number,
-                    ),
-                  ),
-                ]
-            ),
-            const SizedBox(height: 30),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                SizedBox(
-                  width: 300,
-                  child: TextField(
-                    controller: priceController,
-                    decoration: const InputDecoration(
-                      labelText: 'Price',
-                      border: OutlineInputBorder(),
-                    ),
-                    keyboardType: TextInputType.number,
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              // Make Dropdown Search
+              DropdownSearch<String>(
+                items: makes,
+                selectedItem: selectedMake,
+                dropdownDecoratorProps: const DropDownDecoratorProps(
+                  dropdownSearchDecoration: InputDecoration(
+                    labelText: 'Select Make',
+                    border: OutlineInputBorder(),
                   ),
                 ),
-              ],
-            ),
-            const SizedBox(height: 30),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                SizedBox(
-                  width: 300,
-                  child: TextField(
-                    controller: descriptionController,
-                    decoration: const InputDecoration(
-                      labelText: 'Description',
-                      border: OutlineInputBorder(),
-                    ),
-                    keyboardType: TextInputType.text,
+                popupProps: const PopupProps.menu(
+                  showSearchBox: true,
+                ),
+                onChanged: (make) {
+                  setState(() {
+                    selectedMake = make;
+                    models = makesAndModels[make] ?? [];
+                    selectedModel = null;
+                  });
+                },
+              ),
+              const SizedBox(height: 20),
+
+              // Model Dropdown Search
+              DropdownSearch<MMAPI.Model>(
+                items: models,
+                selectedItem: selectedModel,
+                itemAsString: (model) => model.modelName,
+                dropdownDecoratorProps: const DropDownDecoratorProps(
+                  dropdownSearchDecoration: InputDecoration(
+                    labelText: 'Select Model',
+                    border: OutlineInputBorder(),
                   ),
                 ),
-              ],
-            ),
-            const SizedBox(height: 30),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                SizedBox(
-                  width: 300,
-                  child: TextField(
-                    controller: imageController,
-                    decoration: const InputDecoration(
-                      labelText: 'Image',
-                      border: OutlineInputBorder(),
-                    ),
-                    keyboardType: TextInputType.text,
-                  ),
+                popupProps: const PopupProps.menu(
+                  showSearchBox: true,
                 ),
-              ],
-            ),
-            const SizedBox(height: 30),
-            ElevatedButton(
-              onPressed: () {
-                _handleAddToListings();
-              },
-              style: ElevatedButton.styleFrom(
-                backgroundColor: const Color(0xffe23636), // Button color
-                elevation: 5, // Elevation for shadow
-                padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 15),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(2),
-                  side: const BorderSide(color: Colors.white, width: 2), // Border color
+                onChanged: (model) {
+                  setState(() {
+                    selectedModel = model;
+                  });
+                },
+              ),
+              const SizedBox(height: 20),
+
+              // Year TextField
+              TextField(
+                controller: yearController,
+                decoration: const InputDecoration(
+                  labelText: 'Year',
+                  border: OutlineInputBorder(),
+                ),
+                keyboardType: TextInputType.number,
+              ),
+              const SizedBox(height: 20),
+
+              // Price TextField
+              TextField(
+                controller: priceController,
+                decoration: const InputDecoration(
+                  labelText: 'Price',
+                  border: OutlineInputBorder(),
+                ),
+                keyboardType: TextInputType.number,
+              ),
+              const SizedBox(height: 20),
+
+              // Description TextField
+              TextField(
+                controller: descriptionController,
+                decoration: const InputDecoration(
+                  labelText: 'Description',
+                  border: OutlineInputBorder(),
+                ),
+                keyboardType: TextInputType.text,
+              ),
+              const SizedBox(height: 20),
+
+              // Image URL TextField
+              TextField(
+                controller: imageController,
+                decoration: const InputDecoration(
+                  labelText: 'Image URL',
+                  border: OutlineInputBorder(),
+                ),
+                keyboardType: TextInputType.url,
+              ),
+              const SizedBox(height: 20),
+
+              // Add to Listings Button
+              ElevatedButton(
+                onPressed: _handleAddToListings,
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color(0xffe23636),
+                  padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 15),
+                ),
+                child: const Text(
+                  'Add to Listings',
+                  style: TextStyle(color: Colors.white),
                 ),
               ),
-              child: const Text(
-                'Add to Listings',
-                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Colors.white),
-              ),
-            ),
-            const SizedBox(height: 30),
-            ElevatedButton(
-              onPressed: () {
-                print('Search button pressed');
-              },
-              style: ElevatedButton.styleFrom(
-                backgroundColor: const Color(0xffe23636), // Button color
-                elevation: 5, // Elevation for shadow
-                padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 15),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(2),
-                  side: const BorderSide(color: Colors.white, width: 2), // Border color
-                ),
-              ),
-              child: const Text(
-                'Save as Draft',
-                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold, color: Colors.white),
-              ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
